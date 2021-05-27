73ed0880dba4c443799110874cff4a6f9d3250ca
jen6 <fjen6b@gmail.com>
2021-05-27 19:22:07 +0900

    fix: shellscript

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cat document_list.csv | while read line
 do
-  NAME=(`echo $line | awk '{print $1}'`)
-  URL=(`echo $line | awk '{print $2}'`)
-  QUERY=(`echo $line | awk '{print $3}'`)
+  NAME=$(echo $line | awk '{print $1}')
+  URL=$(echo $line | awk '{print $2}')
+  QUERY=$(echo $line | awk '{print $3}')
   echo "$NAME"
-  node src/app.js ${URL} ${QUERY} > documents/${NAME}.md
+  node src/app.js "${URL}" "${QUERY}" > documents/"${NAME}".md
 done
 
